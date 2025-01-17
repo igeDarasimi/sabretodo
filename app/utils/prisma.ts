2d643ee48cda0f550be1c2e1016b89c3af642e99
igeDarasimi <93213821+igeDarasimi@users.noreply.github.com>
2025-01-17 18:25:33 +0100

    Update prisma.ts

--- app/utils/prisma.ts
+++ app/utils/prisma.ts
@@ -1,3 +1,17 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient()
+// import { PrismaClient } from "@prisma/client";
+// export const prisma = new PrismaClient()
 
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
