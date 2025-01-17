25b201e2b8d5c8e11fc79ec5b7d0b7009deca1c7
igeDarasimi <93213821+igeDarasimi@users.noreply.github.com>
2025-01-17 20:34:57 +0100

    Update prisma.ts

--- app/utils/prisma.ts
+++ app/utils/prisma.ts
@@ -1,20 +1,37 @@
 // import { PrismaClient } from "@prisma/client";
 // export const prisma = new PrismaClient()
 
+
+// import { PrismaClient } from '@prisma/client';
+
+// const prismaClientSingleton = () => {
+//     return new PrismaClient();
+// };
+
+// declare const globalThis: {
+//     prisma: ReturnType<typeof prismaClientSingleton>;
+//     prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+
+// }  & typeof global;
+
+// export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+
+
+// if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
+
+
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-    return new PrismaClient();
-};
+let prisma: PrismaClient;
 
-declare const globalThis: {
-    prisma: ReturnType<typeof prismaClientSingleton>;
-    prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-}  & typeof global;
-
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
+export default prisma;
