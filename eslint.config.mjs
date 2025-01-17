71cac702f849fbe57ee5308ef3cb9fbacedc4f03
igeDarasimi <93213821+igeDarasimi@users.noreply.github.com>
2025-01-17 13:34:35 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  rules: {
+    "no-unused-vars": "off",
+  },
+}
+
 export default eslintConfig;
